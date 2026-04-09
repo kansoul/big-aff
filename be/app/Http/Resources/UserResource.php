745176f80c8de0826f88c65762943344784f472a
AnLVT-Kozocom <anlvt@kozo-japan.com>
2026-04-09 17:05:49 +0700

    feat: add is_admin field to UserResource and create CreateRoleAdminSeeder for admin role setup

--- be/app/Http/Resources/UserResource.php
+++ be/app/Http/Resources/UserResource.php
@@ -35,6 +35,7 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'permissions' => $permissions,
+            'is_admin' => $this->is_admin,
         ];
     }
 }
---
+++ be/database/seeders/CreateRoleAdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\Permission;
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+/**
+ * CreateRoleAdminSeeder class
+ */
+class CreateRoleAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role = Role::create([
+            'name' => 'Admin',
+            'permissions' => Permission::FULL_ACCESS_SENTINEL,
+        ]);
+
+        $this->command->info("Role \"{$role->name}\" created successfully! id: {$role->id}");
+    }
+}
